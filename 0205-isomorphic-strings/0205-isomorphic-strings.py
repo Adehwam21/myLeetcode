94380402cd94b63596c6bd148ac9de7916ef8ed5
Adehwam21 <100027234+Adehwam21@users.noreply.github.com>
2023-03-28 03:38:36 +0000

    Time: 38 ms (81.61%), Space: 14.2 MB (81.55%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -2,8 +2,7 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         s_t, t_s = {},{}
         
-        for i in range(len(s)):
-            char1, char2 = s[i],t[i]
+        for char1, char2 in zip(s,t):
             if(char1 in s_t and s_t[char1] != char2)or(
                 char2 in t_s and t_s[char2] != char1):
                 return False 
