b9508541b1ec96ad6ba4700b41fdc5d650e751bb
Aaron  Katey Kudadjie <100027234+Adehwam21@users.noreply.github.com>
2024-03-29 18:06:05 +0000

    Time: 132 ms (92.54%), Space: 17 MB (82.20%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -1,5 +1,17 @@
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        return [set(nums1) - set(nums2), set(nums2) - set(nums1)]
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+        a, b = [], []
+        
+        disjoint = nums1.symmetric_difference(nums2)
+        
+        for i in disjoint:
+            if i in nums1:
+                a.append(i)
+            else:
+                b.append(i)
+        return ([a,b])
+            
         
         
