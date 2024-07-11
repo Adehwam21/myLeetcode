315a8910872e2db45bae07630777c17db599566b
Aaron  Katey Kudadjie <100027234+Adehwam21@users.noreply.github.com>
2024-07-11 02:40:16 +0000

    Time: 46 ms, Memory: 16.5 MB - LeetHub

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -1,24 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        valid_pair = {
-            ')': '(',
-            ']': '[',
-            '}': '{'
-        }
-        
+        valid_pair = {')': '(', ']': '[', '}': '{'}
         stack = []
-        open_par = valid_pair.values()
-        close_par = valid_pair.keys()
         
         for char in s:
-            if char in open_par:
+            if char in valid_pair.values():
                 stack.append(char)
-            elif char in close_par:
+            elif char in valid_pair.keys():
                 if stack and stack[-1] == valid_pair[char]:
                     stack.pop()
                 else:
                     return False
-            else:
-                return False
         
-        return not stack
+        return not stack # If true, it means all open parentheses were matched correctly because hence poped, otherwise, there is still in invalid sequence so the string will not be a valid parentheses
+
