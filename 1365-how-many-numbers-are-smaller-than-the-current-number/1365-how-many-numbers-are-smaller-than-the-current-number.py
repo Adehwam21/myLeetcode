3803508f08e74b11fe5357fc731db9b957bbd89a
Aaron  Katey Kudadjie <100027234+Adehwam21@users.noreply.github.com>
2024-07-14 13:46:06 +0000

    Time: 43 ms (97.86%), Space: 16.5 MB (57.74%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,14 +1,26 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        nums_copy = sorted(nums)
-        count_dict = {}
-        
-        for i in range(len(nums_copy)):
-            if nums_copy[i] not in count_dict:
-                count_dict[nums_copy[i]] = i
-        
+        max_val = max(nums)
+        freq = [0] * (max_val + 1)
         result = []
-        for num in nums:
-            result.append(count_dict[num])
         
+        # Frequency array to count number of times a number occurs in nums
+        # The number in nums is represented by the index and the count is the
+        # number in that index
+        
+        for num in nums:
+            freq[num] += 1
+        
+        # Frequency array is used to create a prefix sum array 
+        for i in range(1, len(freq)):
+            freq[i] += freq[i - 1]
+        
+        # The element before index of num in the prefix sum array 
+        # serves as the number of elements in nums list that are lesser than num
+
+        for num in nums:
+            if num == 0:
+                result.append(0) # to handle index out of range exception
+            else:
+                result.append(freq[num - 1])
         return result
