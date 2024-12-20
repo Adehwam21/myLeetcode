cfd2171094c844948b588260126c331ac824f961
MrUzumaki <100027234+Adehwam21@users.noreply.github.com>
2024-12-20 07:16:39 +0000

    Time: 15 ms (18.56%), Space: 19.4 MB (11.44%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,16 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counts = {}
-        majority = 0
+        count_dict = {}
         threshold = len(nums) / 2
 
         for i in range(0, len(nums)):
-            if nums[i] in counts:
-                counts[nums[i]] += 1
-                if counts[nums[i]] > threshold:
+            if nums[i] in count_dict:
+                count_dict[nums[i]] += 1
+                if count_dict[nums[i]] > threshold:
                     return nums[i]
             else:
-                counts[nums[i]] = 1
-                if counts[nums[i]] > threshold:
+                count_dict[nums[i]] = 1
+                if count_dict[nums[i]] > threshold:
                     return nums[i]
-        
