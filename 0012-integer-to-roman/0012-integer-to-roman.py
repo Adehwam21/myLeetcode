d3bb2adf2ef45e0ba396902555a7145b2db4c343
MrUzumaki <100027234+Adehwam21@users.noreply.github.com>
2024-12-26 01:52:43 +0000

    Time: 3 ms (79.94%), Space: 18 MB (5.83%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,50 +1,13 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        rom_map = {
-            1: "I", 5: "V", 10: "X",
-            50: "L", 100: "C",
-            500: "D", 1000: "M"
-        }
-        num_parts = [(char) + ("0" * i) for i, char in enumerate(str(num)[::-1])]
-        roman_parts = ""  
-        
-        for i, val in enumerate(num_parts):
-            if i == 0:  # Units place
-                maxi = 5
-                mini = 1
-                if val[0] == "4":
-                    roman_parts = "IV" + roman_parts
-                elif val[0] == "9":
-                    roman_parts = "IX" + roman_parts
-                else:
-                    rem = int(val)  
-                    roman = (rom_map[maxi] if rem >= maxi else "") + rom_map[mini] * (rem % maxi // mini)
-                    roman_parts = roman + roman_parts
-            elif i == 1:  # Tens place
-                maxi = 50
-                mini = 10
-                if val[0] == "4":
-                    roman_parts = "XL" + roman_parts
-                elif val[0] == "9":
-                    roman_parts = "XC" + roman_parts
-                else:
-                    rem = int(val)  
-                    roman = (rom_map[maxi] if rem >= maxi else "") + rom_map[mini] * (rem % maxi // mini)
-                    roman_parts = roman + roman_parts
-            elif i == 2:  # Hundreds place
-                maxi = 500
-                mini = 100
-                if val[0] == "4":
-                    roman_parts = "CD" + roman_parts
-                elif val[0] == "9":
-                    roman_parts = "CM" + roman_parts
-                else:
-                    rem = int(val) 
-                    roman = (rom_map[maxi] if rem >= maxi else "") + rom_map[mini] * (rem % maxi // mini)
-                    roman_parts = roman + roman_parts
-            else:  # Thousands place
-                maxi = 1000
-                roman = rom_map[maxi] * (int(val) // maxi)
-                roman_parts = roman + roman_parts
-        
+        value_symbols = [
+            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
+        ]
+        roman_parts = ""
+        for value, symbol in value_symbols:
+            while num >= value:
+                roman_parts += symbol
+                num -= value
         return roman_parts
