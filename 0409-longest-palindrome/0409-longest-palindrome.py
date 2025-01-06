2e3074d68a120980ecd581e93f3d5db0288ac944
MrUzumaki <100027234+Adehwam21@users.noreply.github.com>
2025-01-06 13:54:39 +0000

    Time: 34 ms (4.09%), Space: 13.9 MB (100%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,3 +1,29 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
+        char_map = {}
+    
+        # maping charactes with their counts
+        for char in s:
+            if char not in char_map:
+                char_map[char] = 1
+            else:
+                char_map[char] += 1
+                
+        total,odd = 0,0
+        
+        if len(char_map) == 1:
+            return char_map[s[0]]
+        
+        for i in char_map.values():
+            if i > 1:
+                if i % 2 == 0:
+                    total += i
+                else:
+                    total += i-1
+                    odd += 1
+            else:
+                odd += 1
+        if odd > 0:
+            total += 1
+        return total
         
