df906212c3c162730071c8c68e1ccb20eab07f74
MrUzumaki <100027234+Adehwam21@users.noreply.github.com>
2025-01-07 04:00:22 +0000

    Time: 0 ms (100%), Space: 17.8 MB (19.52%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
@@ -1,9 +1,5 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
-        match = []
-        for word1 in words:
-            for word2 in words:
-                if word1 not in match and len(word1) < len(word2):
-                    if word1 in word2:
-                        match.append(word1)
-        return match
+        r=' '.join(words)
+        s=[i for i in words if r.count(i)>1]
+        return s
