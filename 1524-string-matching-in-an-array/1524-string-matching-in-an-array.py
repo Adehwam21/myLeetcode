5e7daaff180c92a802071c65ed57fcd87ae1359c
MrUzumaki <100027234+Adehwam21@users.noreply.github.com>
2025-01-07 03:57:20 +0000

    Time: 12 ms (8.18%), Space: 18 MB (6.61%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
@@ -3,7 +3,7 @@ class Solution:
         match = []
         for word1 in words:
             for word2 in words:
-                if word1 in word2 and len(word1) < len(word2) and word1 not in match:
-                    match.append(word1)
-        
+                if word1 not in match and len(word1) < len(word2):
+                    if word1 in word2:
+                        match.append(word1)
         return match
